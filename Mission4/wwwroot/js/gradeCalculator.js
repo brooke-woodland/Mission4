8248007f2204d1f3eecac646c58dd87e7d1be5ca
wb385334 <brooke@woodlandfamily.org>
2023-01-30 22:35:52 -0700

    jquery

--- Mission4/wwwroot/js/gradeCalculator.js
+++ Mission4/wwwroot/js/gradeCalculator.js
@@ -1,51 +1,52 @@
-﻿document.getElementById('calculate').addEventListener('click', function () {
+﻿$('#calculate').click(function () {
 
     var letterGrade = "A";
     var numericGrade = 100;
 
     //get the values from the form
 
-    assignment = document.getElementById('assignment').value;
-    group = document.getElementById('group').value;
-    quiz = document.getElementById('quiz').value;
-    midterm = document.getElementById('midterm').value;
-    final = document.getElementById('final').value;
-    intex = document.getElementById('intex').value;
+    assignment = $('#assignment').val();
+    group = $('#group').val();
+    quiz = $('#quiz').val();
+    midterm = $('#midterm').val();
+    final = $('#final').val();
+    intex = $('#intex').val();
 
     //calculate grade
 
-    numericGrade = ((assignment * .5) + (group * .1) + (quiz * .1) + (midterm * .1) + (final * .1) + (intex * .1));
+    numericGrade = (parseInt(assignment) * .5) + (parseInt(group) * .1) + (parseInt(quiz) * .1) + (parseInt(midterm) * .1) + (parseInt(final) * .1) + (parseInt(intex) * .1);
 
+    alert(numericGrade);
     //determine the letter grade
 
     if (numericGrade >= 94) {
         letterGrade = "A";
     } else if (numericGrade >= 90) {
-        letterGrade = "A-"
+        letterGrade = "A-";
     } else if (numericGrade >= 87) {
-        letterGrade = "B+"
+        letterGrade = "B+";
     } else if (numericGrade >= 84) {
-        letterGrade = "B"
+        letterGrade = "B";
     } else if (numericGrade >= 80) {
-        letterGrade = 'B-'
+        letterGrade = 'B-';
     } else if (numericGrade >= 77) {
-        letterGrade = 'C+'
+        letterGrade = 'C+';
     } else if (numericGrade >= 74) {
-        letterGrade = 'C'
+        letterGrade = 'C';
     } else if (numericGrade >= 70) {
-        letterGrade = 'C-'
+        letterGrade = 'C-';
     } else if (numericGrade >= 67) {
-        letterGrade = 'D+'
+        letterGrade = 'D+';
     } else if (numericGrade >= 64) {
-        letterGrade = 'D'
+        letterGrade = 'D';
     } else if (numericGrade >= 60) {
-        letterGrade = 'D-'
+        letterGrade = 'D-';
     } else {
-        letterGrade = 'E'
+        letterGrade = 'E';
     };
 
-    // post results on the page
+    // write results on the page
 
-    document.getElementById('letterGrade').innerHTML = letterGrade;
-    document.getElementById('numericGrade').innerHTML = numericGrade;
+    $('#letterGrade').html( "Letter Grade: " + String(letterGrade));
+    $('#numericGrade').html("Numeric Grade: " + String(numericGrade));
         });
